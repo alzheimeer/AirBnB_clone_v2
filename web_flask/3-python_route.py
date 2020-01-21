1be4f844c7446d5d1f0400d85d25093f3ea5de26
alzheimeer <fogniebla@hotmail.com>
2020-01-21 11:46:22 -0800

    feature task 3 route with or without /

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" flask """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def h0():
+    '''print'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def h1():
+    '''print2'''
+    return 'HBNB'
+
+
+@app.route('/c/<name>')
+def h2(name):
+    '''print2'''
+    return 'C {}'.format(name.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<name>')
+def h3(name='is cool'):
+    '''print3'''
+    return 'Python {}'.format(name.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
