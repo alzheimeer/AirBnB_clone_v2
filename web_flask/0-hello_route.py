995b025fc8d9ad0d500b72929dbfd31a0983ed63
alzheimeer <fogniebla@hotmail.com>
2020-01-21 10:42:55 -0800

    fix task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+""" flask """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello():
+    '''print'''
     return 'Hello HBNB!'
 
-app.run()
+if __name__ == '__main__':
+    app.run()
