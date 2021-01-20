38db73a3cecc5404ffc8acd806af8c09f654c601
alzheimeer <fogniebla@hotmail.com>
2021-01-20 14:55:49 -0800

    fix error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" flask """
+""" Starts Flask. """
 from flask import Flask, render_template
 from models import storage
+from models import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    '''close storage'''
+def close_storage(self):
+    """ Remove the current SQLAlchemy Session. """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def h1():
-    '''config html route and featch data'''
+    """ List States """
     s = list(storage.all('State').values())
     s.sort(key=lambda state: state.name)
     return render_template('7-states_list.html', list1=s)
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(port=5000)
